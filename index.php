e834c6a0ad6410096238b03100f7bf21be36d353
Yannyezixin <646840080@qq.com>
2014-06-19 20:19:39 +0800

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>Simple Chat | Yann</title>
+</head>
+<body>
+    <form id="form">
+        <label for="name">Name:</label>
+        <input id="name" type="text" name="name"><br />
+        <label for="phone">Phone:</label>
+        <input id="phone" type="text" name="phone"><br />
+        <label for="address">Address:</label>
+        <input id="address" type="text" name="address"><br />
+        <input type="submit" value="Send">
+    </form>
+</body>
+</html>
